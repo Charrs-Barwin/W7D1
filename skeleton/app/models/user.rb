b1930df1e004dc4057735a6d9045cb859a3fb77c
Charrs-Barwin <mikehollander84@gmail.com>
2022-05-09 22:24:33 -0400

    fixed migration, set empty model methods, and empty contollers

--- skeleton/app/models/user.rb
+++ skeleton/app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ApplicationRecord
+    attr_reader :password
+    validates :user_name, :session_token, presence: true
+
+    validates :password_digest, presence: { message: 'Password can\'t be blank' }
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    def reset_session_token!
+
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+
+    end
+
+    def self.find_by_credentials(user_name, password)
+
+    end
 end
